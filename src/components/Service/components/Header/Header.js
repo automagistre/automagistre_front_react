ed6b25e26b55e134df4d4e2e4d009f4519f326ae
Kirill Sidorov <kirillsidorov@gmail.com>
2019-03-07 11:59:58 +0300

    Animation MobMenu toggle

--- src/components/Service/components/Header/Header.js
+++ src/components/Service/components/Header/Header.js
@@ -7,28 +7,43 @@ import Switch from "./Switch/Switch";
 import Links from "./Links/Links";
 import MobMenu from "./MobMenu/MobMenu";
 
-const Header = () => {
-    return (
-        <header className="site-header" id="site-header">
-            <div className="sh-line" id="header-line">
-                <div className="sh-line__row sh-line__for-comp">
-                    <div className="sh-line__col-lt">
-                        <Logo />
-                        <DropMenu place = 'Header'/>
+class Header extends React.Component {
+
+    state= {
+        isOpen: false
+    }
+
+    toggleMenuHandler = () => {
+        this.setState({isOpen: !this.state.isOpen})
+    }
+
+    render() {
+        let cls = 'site-header'
+        if (this.state.isOpen) {
+            cls += ' is-open'
+        }
+        return (
+            <header className={cls} id="site-header">
+                <div className="sh-line" id="header-line">
+                    <div className="sh-line__row sh-line__for-comp">
+                        <div className="sh-line__col-lt">
+                            <Logo />
+                            <DropMenu place = 'Header'/>
+                        </div>
+                        <Switch />
+                        <Links />
                     </div>
-                    <Switch />
-                    <Links />
+                    <div className="sh-line__row sh-line__for-mob">
+                        <Logo mob={true}/>
+                        <MobMenu open={this.state.isOpen} toggleMenuHandler={this.toggleMenuHandler}/>
+                    </div>
                 </div>
-                <div className="sh-line__row sh-line__for-mob">
-                    <Logo mob={true}/>
-                    <MobMenu />
-                </div>
-            </div>
 
-            <Nav />
+                <Nav />
 
-        </header>
-    )
-};
+            </header>
+        )
+    }
+}
 
 export default Header;
--- src/components/Service/components/Header/MobMenu/MobMenu.js
+++ src/components/Service/components/Header/MobMenu/MobMenu.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import '../Header.css'
+import './MobMenu.css'
 import {companyContacts} from '../../../../../vars/company'
 
-const MobMenu = () => {
+const MobMenu = (props) => {
+    let cls = "mobmenu-btn sh-mob__menu-btn js-mobmenu-toggle"
+    if (props.open) {
+        cls += " is-open"
+    }
     return (
         <div className="sh-mob__col-rt">
             <a className="sh-mob__phone" href={`tel:${companyContacts.phoneLink}`} />
-            <a className="mobmenu-btn sh-mob__menu-btn js-mobmenu-toggle" role="button">
+            <a className={cls} role="button" onClick={props.toggleMenuHandler} >
                 <span className="_top" />
                 <span className="_middle" />
                 <span className="_bottom" />
