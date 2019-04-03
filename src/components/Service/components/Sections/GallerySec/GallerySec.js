85186d65703cc386e48b2ec13d1b610d94b9d28c
Kirill Sidorov <kirillsidorov@gmail.com>
2019-04-03 16:41:28 +0300

    Gallery Refactoring

--- src/components/Service/components/Sections/GallerySec/GallerySec.js
+++ src/components/Service/components/Sections/GallerySec/GallerySec.js
@@ -3,47 +3,72 @@ import './GallerySec.less'
 import {companyAbout} from "../../../../../vars/company";
 
 import Facts from "../Facts/Facts";
-import ImageItem from "./ImageItem/ImageItem";
+
+import Zoom from 'react-medium-image-zoom'
+import './block_zoom-pop.less'
+
+const ImageItem = props => {
+    const clsImgSize = 'sec-gallery__cell-' + props.size;
+    return(
+        <div className={clsImgSize}>
+            <Zoom image={{
+                src: props.imgSrc,
+                alt: props.imgAlt,
+                className: 'sec-gallery__img',
+            }}
+                  zoomImage={{
+                      alt: props.imgAlt,
+                      className: 'sec-gallery__img zoom-img',
+                  }}
+                  defaultStyles={{
+                      zoomImage:{
+                          cursor: "pointer",
+                      },
+                      image:{
+                          cursor: "pointer"
+                      },
+                      overlay:{
+                          opacity: 0.9
+                      }
+                  }}
+            />
+        </div>
+    )
+};
 
 
-
-class GallerySec extends React.Component {
-    IMG_DIR = '/images/sec-gallery/';
-
-    render() {
-        return(
-            <section className="sec-gallery js-sec-scroll">
-                <div className="sec-gallery__row js-scroll-x">
-                    <div className="sec-gallery__col">
-                        <ImageItem  size={100} imgSrc={`${this.IMG_DIR}work_01.jpg`} imgAlt="Автосервис"/>
-                        <div className="sec-gallery__text sec-gallery__text_top-rt">
-                            <h2 className="sec-gallery__title">{companyAbout.title}</h2>
-                            <div className="sec-gallery__note">
-                                {companyAbout.note}
-                            </div>
+const GallerySec = () => {
+    const IMG_DIR = '/images/sec-gallery/';
+    return(
+        <section className="sec-gallery js-sec-scroll">
+            <div className="sec-gallery__row js-scroll-x">
+                <div className="sec-gallery__col">
+                    <ImageItem  size={100} imgSrc={`${IMG_DIR}work_01.jpg`} imgAlt="Автосервис"/>
+                    <div className="sec-gallery__text sec-gallery__text_top-rt">
+                        <h2 className="sec-gallery__title">{companyAbout.title}</h2>
+                        <div className="sec-gallery__note">
+                            {companyAbout.note}
                         </div>
-                    </div>
-                    <div className="sec-gallery__col">
-                        <ImageItem  size={50} imgSrc={`${this.IMG_DIR}work_02.jpg`} imgAlt="Автосервис"/>
-                        <ImageItem  size={50} imgSrc={`${this.IMG_DIR}work_03.jpg`} imgAlt="Автосервис"/>
-                        <div className="sec-gallery__text sec-gallery__text_btm-rt">
-                            <Facts theme="sec-gallery__facts"/>
-                        </div>
-                    </div>
-                    <div className="sec-gallery__col">
-                        <ImageItem  size={33} imgSrc={`${this.IMG_DIR}work_04.jpg`} imgAlt="Автосервис"/>
-                        <ImageItem  size={67} imgSrc={`${this.IMG_DIR}work_05.jpg`} imgAlt="Автосервис"/>
-                    </div>
-                    <div className="sec-gallery__col">
-                        <ImageItem  size={50} imgSrc={`${this.IMG_DIR}work_02.jpg`} imgAlt="Автосервис"/>
-                        <ImageItem  size={50} imgSrc={`${this.IMG_DIR}work_03.jpg`} imgAlt="Автосервис"/>
                     </div>
                 </div>
-            </section>
-        )
-    }
-
-
+                <div className="sec-gallery__col">
+                    <ImageItem  size={50} imgSrc={`${IMG_DIR}work_02.jpg`} imgAlt="Автосервис"/>
+                    <ImageItem  size={50} imgSrc={`${IMG_DIR}work_03.jpg`} imgAlt="Автосервис"/>
+                    <div className="sec-gallery__text sec-gallery__text_btm-rt">
+                        <Facts theme="sec-gallery__facts"/>
+                    </div>
+                </div>
+                <div className="sec-gallery__col">
+                    <ImageItem  size={33} imgSrc={`${IMG_DIR}work_04.jpg`} imgAlt="Автосервис"/>
+                    <ImageItem  size={67} imgSrc={`${IMG_DIR}work_05.jpg`} imgAlt="Автосервис"/>
+                </div>
+                <div className="sec-gallery__col">
+                    <ImageItem  size={50} imgSrc={`${IMG_DIR}work_02.jpg`} imgAlt="Автосервис"/>
+                    <ImageItem  size={50} imgSrc={`${IMG_DIR}work_03.jpg`} imgAlt="Автосервис"/>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default GallerySec
