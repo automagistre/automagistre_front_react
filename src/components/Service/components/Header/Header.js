72a432376dc19dbf61d32100286e5a6f165cd240
Kirill Sidorov <kirillsidorov@gmail.com>
2019-04-04 20:52:51 +0300

    Header animation

--- src/components/Service/components/Header/Header.js
+++ src/components/Service/components/Header/Header.js
@@ -9,38 +9,74 @@ import MobMenu from "./MobMenu/MobMenu";
 
 class Header extends React.Component {
 
-    state= {
-        isOpen: false
+    lastScrollY = 0
+    navBarHeight = 0
+
+    state = {
+        DropMenuIsOpen: false,
+        isScrolled: false,
+        navDown: true
+    }
+
+    componentDidMount() {
+        document.addEventListener('scroll', this.toggleHeader)
+        this.navBarHeight = document.getElementById('header-line').offsetHeight
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('scroll', this.toggleHeader)
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        this.navBarHeight = document.getElementById('header-line').offsetHeight
+    }
+
+    toggleHeader =() => {
+        let currentScroll = window.pageYOffset || document.documentElement.scrollTop,
+            delta = 5
+        if (Math.abs(this.lastScrollY - currentScroll) <= delta) return
+        if (currentScroll > 150) {
+            this.setState({isScrolled: true})
+        } else {
+            this.setState({isScrolled: false})
+        }
+        if (currentScroll > this.lastScrollY && currentScroll > this.navBarHeight * 5) {
+            this.setState({navDown: false})
+        } else {
+            if (currentScroll + window.innerHeight < document.body.scrollHeight) {
+                this.setState({navDown: true})
+            }
+        }
+        this.lastScrollY = currentScroll
     }
 
     toggleMenuHandler = () => {
-        this.setState({isOpen: !this.state.isOpen})
+        this.setState({DropMenuIsOpen: !this.state.DropMenuIsOpen})
     }
 
     render() {
-        let cls = 'site-header'
-        if (this.state.isOpen) {
-            cls += ' is-open'
-        }
+        let cls = 'site-header',
+            navState = 'sh-line'
+        if (this.state.DropMenuIsOpen) { cls += ' is-open' }
+        if (this.state.isScrolled) { navState += ' is-scrolled'}
+        this.state.navDown ? navState += ' nav-down' : navState += ' nav-up'
         return (
             <header className={cls} id="site-header">
-                <div className="sh-line" id="header-line">
+                <div className={navState} id="header-line">
                     <div className="sh-line__row sh-line__for-comp">
                         <div className="sh-line__col-lt">
                             <Logo />
-                            <DropMenu place = 'Header'/>
+                            <DropMenu place='Header'/>
                         </div>
                         <Switch />
                         <Links />
                     </div>
                     <div className="sh-line__row sh-line__for-mob">
                         <Logo mob={true}/>
-                        <MobMenu open={this.state.isOpen} toggleMenuHandler={this.toggleMenuHandler}/>
+                        <MobMenu open={this.state.DropMenuIsOpen} toggleMenuHandler={this.toggleMenuHandler}/>
                     </div>
                 </div>
-
                 <Nav />
-
             </header>
         )
     }
