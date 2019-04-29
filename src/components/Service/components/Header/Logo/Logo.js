2e4a1f43065a5a6f620ef0f9c7e791d2734e480f
Kirill Sidorov <kirillsidorov@gmail.com>
2019-04-29 19:19:27 +0300

    Logo URL

--- src/components/Service/components/Header/Logo/Logo.js
+++ src/components/Service/components/Header/Logo/Logo.js
@@ -7,31 +7,35 @@ import logoWhite from '../../../../../img/logo_magistr_white.svg'
 import logoDarkMob from '../../../../../img/logo_magistr-mob_dark.svg'
 import logoWhiteMob from '../../../../../img/logo_magistr-mob_white.svg'
 import {NavLink} from "react-router-dom";
+import {connect} from "react-redux";
 
 const Logo = (props) => {
-    if (props.mob) {
-        return (
-            <div className="sh-mob__col-lt">
-                <NavLink className="sh-mob__logo" to="/service">
-                    <img className="sh-mob__logo-img sh-logo__img_white"
-                         src={ logoWhiteMob }
-                         alt={companyNameRu}/>
-                    <img className="sh-mob__logo-img  sh-logo__img_dark"
-                         src={ logoDarkMob }
-                         alt={companyNameRu} />
-                </NavLink>
-            </div>
-        )
-    }
-    return (
-        <div className="sh-logo">
-            <img className="sh-logo__img sh-logo__img_white" src={ logoWhite }
-                 alt={companyNameRu} />
-            <img className="sh-logo__img sh-logo__img_dark" src={ logoDark }
-                 alt={companyNameRu} />
-            <NavLink className="sh-logo__link" to="/" />
+    const url = '/service/' + props.manufacture
+    return props.mob ? (
+        <div className="sh-mob__col-lt">
+            <NavLink className="sh-mob__logo" to={url}>
+                <img className="sh-mob__logo-img sh-logo__img_white"
+                     src={logoWhiteMob}
+                     alt={companyNameRu}/>
+                <img className="sh-mob__logo-img  sh-logo__img_dark"
+                     src={logoDarkMob}
+                     alt={companyNameRu}/>
+            </NavLink>
         </div>
-    )
+    ) : (
+        <div className="sh-logo">
+            <img className="sh-logo__img sh-logo__img_white" src={logoWhite}
+                 alt={companyNameRu}/>
+            <img className="sh-logo__img sh-logo__img_dark" src={logoDark}
+                 alt={companyNameRu}/>
+            <NavLink className="sh-logo__link" to={url}/>
+        </div>
+    );
 };
 
-export default Logo
+function mapStateToProps(state) {
+    return {
+        manufacture: state.service.manufacture
+    }
+}
+export default connect(mapStateToProps)(Logo)
