d68aae82d9923dff54344d60ee9991a330683a37
Kirill Sidorov <kirillsidorov@gmail.com>
2019-04-29 19:29:25 +0300

    Links header

--- src/components/Service/components/Header/Links/Links.js
+++ src/components/Service/components/Header/Links/Links.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../Header.less'
 import {companyContacts} from "../../../../../vars/company";
+import {Link} from "react-router-dom";
 
 
 const Links = () => {
@@ -8,10 +9,10 @@ const Links = () => {
         <div className="sh-line__col-rt">
             <ul className="sh-links">
                 <li className="sh-links__item">
-                    <a className="sh-links__link" href="#">Мой гараж</a>
+                    <Link className="sh-links__link" to="/garage">Мой гараж</Link>
                 </li>
                 <li className="sh-links__item">
-                    <a className="sh-links__link" href="#">Контакты</a>
+                    <Link className="sh-links__link" to="/contacts">Контакты</Link>
                 </li>
                 <li className="sh-links__item">
                     <a className="sh-links__link is-active" href={`tel:${companyContacts.phoneLink}`}>{companyContacts.phone}</a>
