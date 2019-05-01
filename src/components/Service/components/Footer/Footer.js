1b8fdca28bcbe5499b66d12fc2abd960ea2b7862
Kirill Sidorov <kirillsidorov@gmail.com>
2019-05-01 19:18:44 +0300

    Footer links refactor

--- src/components/Service/components/Footer/Footer.js
+++ src/components/Service/components/Footer/Footer.js
@@ -4,8 +4,10 @@ import SpaceWellLogo from '../../../../img/logo_space-well_red.svg'
 import MagistrLogoDark from '../../../../img/logo_magistr_dark.svg'
 import DropMenu from "../DropMenu/DropMenu";
 import {Link} from "react-router-dom";
+import {setManufacture, setServiceType} from "../../../../store/actions/serviceActions";
+import {connect} from "react-redux";
 
-const Footer = () => {
+const Footer = props => {
     return (
         <footer className="site-footer">
             <div className="sf-top">
@@ -14,18 +16,20 @@ const Footer = () => {
                         <div className="sf-top__col-lt">
                             <div className="sf-logo">
                                 <img className="sf-logo__img" src={MagistrLogoDark} alt="Автомагистр" />
-                                    {/*<a className="sf-logo__link" href="/" />*/}
+                                    <Link className="sf-logo__link"  to={`/market/${props.manufacture}/`}/>
                             </div>
                             <DropMenu place = 'Footer' />
                         </div>
                         <div className="sf-top__col-rt">
                             <nav className="sf-nav">
-                                <Link className="sf-nav__link is-active" to={'/garage/'}>Сервис в Москве</Link>
-                                <a className="sf-nav__link" href="#">Магазин запчастей</a>
-                                <a className="sf-nav__link" href="#">Корпоративным клиентам</a>
-                                <a className="sf-nav__link" href="#">Мой гараж</a>
-                                <a className="sf-nav__link" href="#">Блог</a>
-                                <a className="sf-nav__link" href="#">Контакты</a>
+                                <Link className="sf-nav__link is-active" to={'/garage'}>Сервис в Москве</Link>
+                                <Link className="sf-nav__link" to={`/market/${props.manufacture}/`}>
+                                    Магазин запчастей
+                                </Link>
+                                <Link className="sf-nav__link" to="/corp/">Корпоративным клиентам</Link>
+                                <Link className="sf-nav__link" to="/garage/">Мой гараж</Link>
+                                <Link className="sf-nav__link" to="/blog/">Блог</Link>
+                                <Link className="sf-nav__link" to="/contacts/">Контакты</Link>
                             </nav>
                             <div className="sf-top__info">
                                 <div className="sf-top__text">
@@ -33,10 +37,10 @@ const Footer = () => {
                                 </div>
                                 <div className="sf-creator sf-top__creator">
                                     <div className="sf-creator__note">Сайт разработан:</div>
-                                    <a className="sf-creator__link" href="http://spacewell.ru" target="_blank">
+                                    <Link className="sf-creator__link" to="http://spacewell.ru" target="_blank">
                                         <img className="sf-creator__img" src={SpaceWellLogo}
                                              alt="Space Well" />
-                                    </a>
+                                    </Link>
                                 </div>
                             </div>
                         </div>
@@ -47,14 +51,26 @@ const Footer = () => {
                 <div className="container">
                     <div className="sf-btm__row">
                         <div className="sf-btm__col">Все права защищены © АвтоМагистр</div>
-                        <div className="sf-btm__col"><a href="#">Политика конфиденциальности</a></div>
+                        <div className="sf-btm__col"><Link to="/privacy/">Политика конфиденциальности</Link></div>
                     </div>
                 </div>
             </div>
-            <a className="sf-car-change icon_n-cars js-show-modal" role="button" title="Выбрать машину" />
-            <a className="scroll-top js-scroll-top" title="Вверх" role="button" />
         </footer>
     )
 };
 
-export default Footer
+function mapStateToProps(state) {
+    return {
+        manufacture: state.service.manufacture,
+        serviceType: state.service.serviceType
+    }
+}
+function mapDispatchToProps(dispatch) {
+    return {
+        setManufacture: manufacture => dispatch(setManufacture(manufacture)),
+        setServiceType: serviceType => dispatch(setServiceType(serviceType))
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Footer)
